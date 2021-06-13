2b1a1468c8ef925deb1bab3cfbd47b6bd1495bcc
ahlee9 <84502703+ahlee9@users.noreply.github.com>
2021-06-13 23:38:42 +0900

    Add files via upload

--- semaphore.c
+++ semaphore.c
@@ -3,37 +3,95 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 #include <time.h>
+
+union semun{
+    int val;
+    struct semid_ds *buf;
+    ushort *array;
+};
+
+#define PATH "/home/ahhyeon/thread-safe-counter"
 
 typedef struct __counter_t {
     int value;
-    pthread_mutex_t lock;
+    int semid;
+    key_t key;
+    union semun arg;
 } counter_t;
 
 unsigned int loop_cnt;
 counter_t counter;
 
-void init(counter_t *c) {
+void init(counter_t *c, char *argv[]) {
     c->value = 0;
-    pthread_mutex_init(&c->lock, NULL);
+    c->semid = semget(c->key, 1, 0600 | IPC_CREAT);
+    c->key = ftok(PATH, 'z');
+
+    if (c->key < 0) {
+        perror(argv[0]);
+        exit(1);
+        }
+    if (c->semid < 0) {
+        perror(argv[0]);
+        exit(1);
+        }
 }
 
 void increment(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
+    int semid;
+    struct sembuf s;
+
+    s.sem_num = 0;
+    s.sem_op = -1;
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
+    
     c->value++;
-    pthread_mutex_unlock(&c->lock);
+    
+    s.sem_num = 0;
+    s.sem_op = 1;
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
+    
 }
 
 void decrement(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
+    int semid;
+    struct sembuf s;
+
+    s.sem_num = 0;
+    s.sem_op = -1;
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
+    
     c->value--;
-    pthread_mutex_unlock(&c->lock);
+    
+    s.sem_num = 0;
+    s.sem_op = 1;
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
 }
 
 int get(counter_t *c) {
-    pthread_mutex_lock(&c->lock);
+    int semid;
+    struct sembuf s;
+
+    s.sem_num = 0;
+    s.sem_op = -1;
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
+    
     int rc = c->value;
-    pthread_mutex_unlock(&c->lock);
+    
+    s.sem_num = 0;
+    s.sem_op = 1;
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
+
     return rc;
 }
 
@@ -51,7 +109,7 @@ void *mythread(void *arg)
 }
                                                                              
 int main(int argc, char *argv[])
-{                    
+{      
     clock_t sem_start, sem_end;
     float exe_time;
 
@@ -59,7 +117,11 @@ int main(int argc, char *argv[])
 
     loop_cnt = atoi(argv[1]);
 
-    init(&counter);
+    init(&counter, argv);
+
+    counter.arg.val = 1;
+    semctl(counter.semid, 0, SETVAL, counter.arg);
+    printf("semind = %d\n", counter.semid);
 
     pthread_t p1, p2;
     printf("main: begin [counter = %d]\n", get(&counter));
@@ -73,6 +135,6 @@ int main(int argc, char *argv[])
     sem_end = clock();
     exe_time = sem_end - sem_start;
     printf("Execution time : %.3f\n", exe_time);
-    
+
     return 0;
 }
